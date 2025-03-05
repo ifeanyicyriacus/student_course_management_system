630230b90d5957a1cafffce634ca48a35d98975f
adebayo kehinde <adebayosamuel6667@gmail.com>
2025-03-05 09:35:39 +0100

    test for instructor class

--- src/scms/course.py
+++ src/scms/course.py
@@ -1,17 +1,19 @@
+from scms.enrollment import Enrollment
 from scms.instructor import Instructor
+from scms.student import Student
+
 
 class Course:
-    def __init__(self, course_id: str, course_name: str, course_description: str, instructor: Instructor):
+    def __init__(self, course_id:str, course_name:str, course_description:str, instructor_assigned:Instructor):
         self.course_id = course_id
         self.course_name = course_name
         self.course_description = course_description
-        self.instructor = instructor
-        self.students = []
-        self.grades = {}
+        self.instructor_assigned = instructor_assigned
+        self.enrollments:[Enrollment] = []
 
-    def enroll_student(self, student):
-        self.students.append(student)
-        self.grades[student.user_id] = None
 
-    def assign_grade(self, student, grade):
-        self.grades[student.user_id] = grade
+        def add_student(self, student:Student):
+            pass
+
+        def remove_student(self, student:Student):
+            pass
--- test/test_instructor.py
+++ test/test_instructor.py
@@ -1,21 +1,21 @@
-import unittest
+from unittest import TestCase
 from instructor import Instructor
 from course import Course
 from student import Student
 
-class TestInstructor(unittest.TestCase):
+class TestInstructor(TestCase):
 
     def test_create_course(self):
-        instructor = Instructor(1, "John Doe", "john@example.com", "password123")
+        instructor = Instructor(1, "adebayo kehinde", "adebayosamuel6667@gmail.com", "password123")
         course = Course("Python 101")
         instructor.create_course(course)
         self.assertIn(course, instructor.courses_taught)
 
     def test_view_students(self):
-        instructor = Instructor(1, "John Doe", "john@example.com", "password123")
-        course = Course("Python 101")
-        student1 = Student("Alice")
-        student2 = Student("Bob")
+        instructor = Instructor(1, "adebayo", "adebayosamuel6667@gmail.com", "password123")
+        course = Course("Python ")
+        student1 = Student("samuel")
+        student2 = Student("ayo")
         course.students.append(student1)
         course.students.append(student2)
         instructor.create_course(course)
