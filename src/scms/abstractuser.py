7db88f3e6d76ff2cfc8f2a70c342578287c08074
adebayo kehinde <adebayosamuel6667@gmail.com>
2025-03-06 22:22:23 +0100

    user class completed

--- src/scms/abstractuser.py
+++ src/scms/abstractuser.py
@@ -1,39 +1,40 @@
 class User:
-    def __init__(self, full_name:str, password:str, email:str, user_type:str):
+    def __init__(self, full_name: str, password: str, email: str, type: str):
         self.full_name = full_name
         self.email = email
         self._password = password
-        self.user_type = user_type
-
-    @property
-    def user_id(self) -> str:
-        return self.__USER_ID
+        self.user_type = type
 
     @property
     def full_name(self) -> str:
         return self.__full_name
+
     @full_name.setter
-    def full_name(self, full_name:str):
+    def full_name(self, full_name: str):
         self.__full_name = full_name
 
     @property
     def email(self) -> str:
         return self.__email
+
     @email.setter
-    def email(self, email:str):
+    def email(self, email: str):
         self.__email = email
 
     @property
     def _password(self) -> str:
         return self.__password
+
     @_password.setter
-    def _password(self, password:str):
+    def _password(self, password: str):
         self.__password = password
 
     @property
-    def user_type(self) -> UserType:
+    def type(self) -> str:
         return self.__user_type
-    @user_type.setter
-    def user_type(self, user_type:UserType):
-        self.__user_type = user_type
+
+    @type.setter
+    def type(self, type: str):
+        self.__user_type = type
+
 
