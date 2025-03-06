5520941a4e87990a531777dd83bf3806245263c7
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-06 08:17:10 +0100

    minor changes

--- src/scms/course.py
+++ src/scms/course.py
@@ -10,7 +10,7 @@ class Course:
         self.course_name = course_name
         self.course_description = course_description
         self.instructor_assigned = instructor_assigned
-        self.enrollments:list = []
+        self.enrolled_student:list = []
 
     @property
     def course_id(self):
@@ -22,6 +22,24 @@ class Course:
     @property
     def course_name(self):
         return self.__course_id
+    @course_name.setter
+    def course_name(self, value):
+        self._course_name = value
+
+    @property
+    def course_description(self):
+        return self.__course_description
+    @course_description.setter
+    def course_description(self, value):
+        self.__course_description = value
+
+    @property
+    def instructor_assigned(self):
+        return self.__instructor_assigned
+    @instructor_assigned.setter
+    def instructor_assigned(self, value):
+        self.__instructor_assigned = value
+
 
     def add_student(self, student):
         pass
---
+++ test/scms/test_instructor.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from scms.instructor import Instructor
+from scms.course import Course
+from scms.student import Student
+
+class TestInstructor(TestCase):
+
+    # def test_create_course(self):
+    #     instructor = Instructor(1, "adebayo kehinde", "adebayosamuel6667@gmail.com", "password123")
+    #     course = Course("Python 101")
+    #     instructor.create_course(course)
+    #     self.assertIn(course, instructor.courses_taught)
+    #
+    # def test_view_students(self):
+    #     instructor = Instructor(1, "adebayo", "adebayosamuel6667@gmail.com", "password123")
+    #     course = Course("Python ")
+    #     student1 = Student("samuel")
+    #     student2 = Student("ayo")
+    #     course.students.append(student1)
+    #     course.students.append(student2)
+    #     instructor.create_course(course)
+    #
+
+
+
