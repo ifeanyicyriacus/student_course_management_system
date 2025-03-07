10c2057b5c5f82734659bd77abbeeac914a2d141
0nyinye <onyinyeekezi2017@gmail.com>
2025-03-07 04:30:38 +0100

    updated cryptography and test_cryptography

--- src/scms/cryptography.py
+++ src/scms/cryptography.py
@@ -1,11 +1,10 @@
-from scms.user import User
 import bcrypt
 
-class Cryptography(User):
+class Cryptography:
     @staticmethod
-    def encrypt(text) -> str:
-        return str(bcrypt.hashpw(text.encode9('utf-8'), bcrypt.gensalt()))
+    def encrypt(text) -> bytes:
+        return bcrypt.hashpw(text.encode('utf-8'), bcrypt.gensalt())
 
     @staticmethod
-    def verify(text:str, encrypted_text:str) -> bool:
-        return bcrypt.checkpw(text.encode('utf-8'), encrypted_text.encode('utf-8'))
+    def verify(text:str, encrypted_text:bytes):
+        return bcrypt.checkpw(text.encode('utf-8'), encrypted_text)
