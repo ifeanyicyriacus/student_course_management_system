a6ba7dad172e33108a613dc283e34f3e6e5af31a
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-08 12:40:08 +0100

    File Handler Update applying MR SK feedback - on dependency injection and teardown of test file after creation

--- test/scms/test_file_handler.py
+++ test/scms/test_file_handler.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import uuid
 
@@ -5,29 +6,38 @@ from src.scms.file_handler import FileHandler
 
 class TestFileHandlerFunctions(unittest.TestCase):
 
+    def setUp(self):
+        random_file_name = str(uuid.uuid4())
+        self.file_path = f"resources/{random_file_name}.txt"
+        self.file_handler = FileHandler(self.file_path)
+        print(self.file_path)
+
+    def tearDown(self):
+        print(self.file_path)
+        os.remove(self.file_path)
+        # ho to delete a file delete file
+
     def test_write_function_writes_a_line_to_file(self):
-        FileHandler.write("resources/hello1.txt", "hello world")
+        self.file_handler.write("hello world")
         expected = ["hello world"]
-        actual = FileHandler.read("resources/hello1.txt")
+        actual = self.file_handler.read()
         self.assertEqual(expected, actual)
 
     def test_read_function_can_write_multiple_line_to_file(self):
-        FileHandler.write("resources/hello.txt", "hello world")
-        FileHandler.write("resources/hello.txt", "how are you")
-        FileHandler.write("resources/hello.txt", "goodbye")
-        actual = FileHandler.read("resources/hello.txt")
+        self.file_handler.write("hello world")
+        self.file_handler.write("how are you")
+        self.file_handler.write("goodbye")
+        actual = self.file_handler.read()
         expected = ["hello world", "how are you", "goodbye"]
         self.assertEqual(expected, actual)
 
     def test_read_function_can_write_to_file_when_file_does_not_exist(self):
-        random_file_name = str(uuid.uuid4())
-        FileHandler.write(f"resources/{random_file_name}.txt", "hello world")
+        self.file_handler.write("hello world")
         expected = ["hello world"]
-        actual = FileHandler.read(f"resources/{random_file_name}.txt")
+        actual = self.file_handler.read()
         self.assertEqual(expected, actual)
 
     def test_read_function_can_read_from_file_when_file_does_not_exist(self):
-        random_file_name = str(uuid.uuid4())
-        actual = FileHandler.read(f"resources/{random_file_name}.txt")
+        actual = self.file_handler.read()
         expected = []
         self.assertEqual(expected, actual)
