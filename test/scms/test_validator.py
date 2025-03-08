fc1309006546ebf770233533246b0d5a0eff639c
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-08 10:57:29 +0100

    added some more test to validator

--- test/scms/test_validator.py
+++ test/scms/test_validator.py
@@ -8,9 +8,12 @@ class TestValidator(TestCase):
     def test_validate_email(self):
         self.assertTrue(self.validator.validate_email("adebayosamuel6667@gmail.com"))
         self.assertFalse(self.validator.validate_email("invalid-email"))
+        self.assertTrue(self.validator.validate_email("username@sub.sub.sub.domain.com"))
+        self.assertTrue(self.validator.validate_email("chibuzo@domain.com.ng.lg"))
+        self.assertTrue(self.validator.validate_email("i@x.com"))
 
     def test_validate_input(self):
         self.assertTrue(self.validator.validate_input("my name"))
         self.assertFalse(self.validator.validate_input("   "))
         self.assertFalse(self.validator.validate_input(""))
-
+        self.assertFalse(self.validator.validate_input(None))
