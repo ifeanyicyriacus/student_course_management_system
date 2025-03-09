57b6724efbf2c54ada7fa55e22b6d3a536f2d9a5
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-09 07:41:56 +0100

    DataLoader class and test this class processes data read from file handler and returns a list of Course, Enrollment, Student and Instructor objects

---
+++ test/scms/test_data_loader.py
@@ -0,0 +1,76 @@
+import os
+from unittest import TestCase
+
+from src.scms.file_handler import FileHandler
+from src.scms.data_loader import DataLoader
+
+
+class TestDataLoader(TestCase):
+    directory = "test"
+    courses_file_path = f"./../data/{directory}/courses.txt"
+    enrollments_file_path = f"./../data/{directory}/enrollments.txt"
+    students_file_path = f"./../data/{directory}/students.txt"
+    instructors_file_path = f"./../data/{directory}/instructors.txt"
+
+    def setUp(self):
+        courses_file_handler = FileHandler(self.courses_file_path)
+        courses_file_handler.write("courseID1, courseName1, courseDescription1, InstructorID1")
+        courses_file_handler.write("courseID2, courseName2, courseDescription2, InstructorID2")
+        courses_file_handler.write("courseID3, courseName3, courseDescription3, InstructorID3")
+        enrollment_file_handler = FileHandler(self.enrollments_file_path)
+        enrollment_file_handler.write("courseID1, studentID1, 78, 2025-03-09")
+        enrollment_file_handler.write("courseID2, studentID1, 70, 2025-02-09")
+        enrollment_file_handler.write("courseID1, studentID2, 80, 2025-01-09")
+        student_file_handler = FileHandler(self.students_file_path)
+        student_file_handler.write("studentID1, fullName1, ifeanyi@email.com, password1")
+        student_file_handler.write("studentID2, fullName2, email2@email.com, password2")
+        instructor_file_handler = FileHandler(self.instructors_file_path)
+        instructor_file_handler.write("InstructorID1, fullName1, chibuzo@email.com, password1")
+        instructor_file_handler.write("InstructorID2, fullName2, sk@email.com, password2")
+        instructor_file_handler.write("InstructorID3, fullName3, chinedu@email.com, password3")
+
+        self.data_loader = DataLoader(self.courses_file_path,
+                                      self.enrollments_file_path,
+                                      self.students_file_path,
+                                      self.instructors_file_path)
+
+    def tearDown(self):
+        os.remove(self.courses_file_path)
+        os.remove(self.enrollments_file_path)
+        os.remove(self.students_file_path)
+        os.remove(self.instructors_file_path)
+
+    def test_get_courses(self):
+        courses = self.data_loader.get_courses()
+        self.assertEqual(len(courses), 3)
+        self.assertEqual(courses[0].course_id, "courseID1")
+        self.assertEqual(courses[0].course_name, "courseName1")
+        self.assertEqual(courses[0].course_description, "courseDescription1")
+        self.assertEqual(courses[0].instructor_id, "InstructorID1")
+        self.assertEqual(courses[2].course_id, "courseID3")
+        self.assertEqual(courses[2].course_name, "courseName3")
+        self.assertEqual(courses[2].course_description, "courseDescription3")
+        self.assertEqual(courses[2].instructor_id, "InstructorID3")
+
+    def test_get_enrollments(self):
+        enrollments = self.data_loader.get_enrollments()
+        self.assertEqual(len(enrollments), 3)
+        self.assertEqual(enrollments[0].course_id, "courseID1")
+        self.assertEqual(enrollments[0].student_id, "studentID1")
+        self.assertEqual(enrollments[0].grade, 78)
+        self.assertEqual(enrollments[0].timestamp, "2025-03-09")
+
+    def test_get_students(self):
+        students = self.data_loader.get_students()
+        self.assertEqual(len(students), 2)
+        self.assertEqual(students[0].student_id, "studentID1")
+        self.assertEqual(students[0].full_name, "fullName1")
+        self.assertEqual(students[0].email, "ifeanyi@email.com")
+        self.assertEqual(students[0]._password, "password1" )
+
+    def test_get_instructors(self):
+        instructors = self.data_loader.get_instructors()
+        self.assertEqual(len(instructors), 3)
+        self.assertEqual(instructors[0].email, "chibuzo@email.com")
+        self.assertEqual(instructors[1].email, "sk@email.com")
+        self.assertEqual(instructors[2].email, "chinedu@email.com")
