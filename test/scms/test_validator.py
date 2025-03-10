21fc45a204aed0eb2a93f705a955a03092861004
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-10 14:19:42 +0100

    Added Password validation to Validation class

--- test/scms/test_validator.py
+++ test/scms/test_validator.py
@@ -19,7 +19,6 @@ class TestValidator(TestCase):
         self.assertFalse(self.validator.validate_input(""))
         self.assertFalse(self.validator.validate_input(None))
 
-
     def test_validate_grade(self):
         self.assertTrue(self.validator.validate_grade("0"))
         self.assertTrue(self.validator.validate_grade("100"))
@@ -30,4 +29,14 @@ class TestValidator(TestCase):
         self.assertFalse(self.validator.validate_grade(-1))
         self.assertFalse(self.validator.validate_grade(101))
 
-
+    def test_validate_password(self):
+        self.assertFalse(self.validator.validate_password("<PASSWORD>"))
+        self.assertFalse(self.validator.validate_password("123456789"))
+        self.assertFalse(self.validator.validate_password("asdfghjkl"))
+        self.assertFalse(self.validator.validate_password("ASDFGHJKL"))
+        self.assertFalse(self.validator.validate_password("         "))
+        self.assertFalse(self.validator.validate_password("~!@#$%^&*()_+"))
+        self.assertFalse(self.validator.validate_password("pAsSw OrD"))
+        self.assertFalse(self.validator.validate_password("PaSsWoRd1"))
+        self.assertTrue(self.validator.validate_password("$<PaSsWoRd123>%"))
+        self.assertTrue(self.validator.validate_password("PaSs  WoRd1"))
